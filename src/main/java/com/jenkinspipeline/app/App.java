865ae6719b17d2750e161c2cd2b1d52498e14889
namrata4447 <83541271+namrata4447@users.noreply.github.com>
2021-05-20 22:48:28 +0530

    updated maven home

--- src/main/java/com/jenkinspipeline/app/App.java
+++ src/main/java/com/jenkinspipeline/app/App.java
@@ -3,17 +3,17 @@ node {
 	    git branch: 'main', credentialsId: 'git_credentials', url: 'https://github.com/namrata4447/jenkins-pipeline-script.git' 
 	}
     stage('Maven-clean') {
-        def mvnHome = tool name: 'maven_home', type: 'maven'
+        def mvnHome = tool name: 'Maven_home', type: 'maven'
         def mvnCMD = "${mvnHome}/bin/mvn"
         sh "${mvnCMD} clean"
 	}
     stage('Maven-compile') {
-        def mvnHome = tool name: 'maven_home', type: 'maven'
+        def mvnHome = tool name: 'Maven_home', type: 'maven'
         def mvnCMD = "${mvnHome}/bin/mvn"
         sh "${mvnCMD} compile"
 	}
     stage('Maven-Package') {
-        def mvnHome = tool name: 'maven_home', type: 'maven'
+        def mvnHome = tool name: 'Maven_home', type: 'maven'
         def mvnCMD = "${mvnHome}/bin/mvn"
         sh "${mvnCMD} package"
 	}
