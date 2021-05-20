9ebc438d9a7cd3942f244fb6a2c4dd95ec336c48
namrata4447 <83541271+namrata4447@users.noreply.github.com>
2021-05-20 22:12:38 +0530

    pipeline script coding

--- src/main/java/com/jenkinspipeline/app/App.java
+++ src/main/java/com/jenkinspipeline/app/App.java
@@ -1,13 +1,20 @@
-package com.jenkinspipeline.app;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+node {
+    stage('SCM-Checkout') {
+	    git branch: 'main', credentialsId: 'git_credentials', url: 'https://github.com/namrata4447/Jenkins-Pipeline.git' 
+	}
+    stage('Maven-clean') {
+        def mvnHome = tool name: 'maven_home', type: 'maven'
+        def mvnCMD = "${mvnHome}/bin/mvn"
+        sh "${mvnCMD} clean"
+	}
+    stage('Maven-compile') {
+        def mvnHome = tool name: 'maven_home', type: 'maven'
+        def mvnCMD = "${mvnHome}/bin/mvn"
+        sh "${mvnCMD} compile"
+	}
+    stage('Maven-Package') {
+        def mvnHome = tool name: 'maven_home', type: 'maven'
+        def mvnCMD = "${mvnHome}/bin/mvn"
+        sh "${mvnCMD} package"
+	}
 }
